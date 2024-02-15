7a43ba55052c49d368405a42581eb7589e9a12f5
Galuh Ramadhan <galuh.html@gmail.com>
2024-02-15 10:46:54 +0700

    ci: application.properties
    mysql configuration

---
+++ database.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE db_simpeg_development;
+
+USE db_simpeg_development;
+
+CREATE TABLE users
+(
+    username    VARCHAR(100) NOT NULL,
+    password    VARCHAR(100) NOT NULL,
+    name        VARCHAR(100) NOT NULL,
+    token       VARCHAR(100),
+    token_expired_at    BIGINT,
+    PRIMARY KEY (username),
+    UNIQUE (token)
+);
