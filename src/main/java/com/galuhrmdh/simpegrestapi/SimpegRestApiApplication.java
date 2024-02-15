a8d615499b1ba31ee49f688fa7fd09242173b794
Galuh Ramadhan <galuh.html@gmail.com>
2024-02-15 10:18:53 +0700

    init commit

---
+++ src/main/java/com/galuhrmdh/simpegrestapi/SimpegRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.galuhrmdh.simpegrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpegRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpegRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galuhrmdh/simpegrestapi/SimpegRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galuhrmdh.simpegrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpegRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
