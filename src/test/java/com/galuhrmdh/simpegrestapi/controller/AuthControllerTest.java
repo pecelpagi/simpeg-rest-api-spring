a6743f4351024fc76b7c8fe04d9364484c917b75
Galuh Ramadhan <galuh.html@gmail.com>
2024-02-16 18:15:05 +0700

    test(auth-controller): login failed wrong password

--- src/test/java/com/galuhrmdh/simpegrestapi/controller/AuthControllerTest.java
+++ src/test/java/com/galuhrmdh/simpegrestapi/controller/AuthControllerTest.java
@@ -2,17 +2,18 @@ package com.galuhrmdh.simpegrestapi.controller;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.galuhrmdh.simpegrestapi.entity.User;
 import com.galuhrmdh.simpegrestapi.model.LoginUserRequest;
 import com.galuhrmdh.simpegrestapi.model.WebResponse;
 import com.galuhrmdh.simpegrestapi.repository.UserRepository;
+import com.galuhrmdh.simpegrestapi.security.BCrypt;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.lang.reflect.Type;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -31,6 +32,11 @@ public class AuthControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @BeforeEach
+    void setUp() {
+        userRepository.deleteAll();
+    }
+
     @Test
     void loginFailedUserNotFound() throws Exception {
         LoginUserRequest request = new LoginUserRequest();
@@ -47,6 +53,31 @@ public class AuthControllerTest {
         ).andDo(result -> {
             WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {
             });
+            assertNotNull(response.getErrors());
+        });
+    }
+
+    @Test
+    void loginFailedWrongPassword() throws Exception {
+        User user = new User();
+        user.setName("Test");
+        user.setUsername("test");
+        user.setPassword(BCrypt.hashpw("test", BCrypt.gensalt()));
+        userRepository.save(user);
+
+        LoginUserRequest request = new LoginUserRequest();
+        request.setUsername("test");
+        request.setPassword("salah");
+
+        mockMvc.perform(
+                post("/api/auth/login")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+        ).andExpectAll(
+                status().isUnauthorized()
+        ).andDo(result -> {
+            WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {});
             assertNotNull(response.getErrors());
         });
     }
