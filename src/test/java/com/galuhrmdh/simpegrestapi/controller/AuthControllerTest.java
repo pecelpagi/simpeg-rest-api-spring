548e8694d8a6658b121045be5242769cc9593208
Galuh Ramadhan <galuh.html@gmail.com>
2024-02-18 09:04:17 +0700

    test(auth-controller): login success

--- src/test/java/com/galuhrmdh/simpegrestapi/controller/AuthControllerTest.java
+++ src/test/java/com/galuhrmdh/simpegrestapi/controller/AuthControllerTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.galuhrmdh.simpegrestapi.entity.User;
 import com.galuhrmdh.simpegrestapi.model.LoginUserRequest;
+import com.galuhrmdh.simpegrestapi.model.TokenResponse;
 import com.galuhrmdh.simpegrestapi.model.WebResponse;
 import com.galuhrmdh.simpegrestapi.repository.UserRepository;
 import com.galuhrmdh.simpegrestapi.security.BCrypt;
@@ -15,7 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -79,6 +80,37 @@ public class AuthControllerTest {
         ).andDo(result -> {
             WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {});
             assertNotNull(response.getErrors());
+        });
+    }
+
+    @Test
+    void loginSuccess() throws Exception {
+        User user = new User();
+        user.setName("Test");
+        user.setUsername("test");
+        user.setPassword(BCrypt.hashpw("test", BCrypt.gensalt()));
+        userRepository.save(user);
+
+        LoginUserRequest request = new LoginUserRequest();
+        request.setUsername("test");
+        request.setPassword("test");
+
+        mockMvc.perform(
+                post("/api/auth/login")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+        ).andExpectAll(
+                status().isOk()
+        ).andDo(result -> {
+                WebResponse<TokenResponse> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<>() {});
+                assertNull(response.getErrors());
+                assertNotNull(response.getData().getToken());
+                assertNotNull(response.getData().getExpiredAt());
+
+                User userDb = userRepository.findById("test").orElse(null);
+                assertNotNull(userDb);
+                assertEquals(userDb.getToken(), response.getData().getToken());
         });
     }
 }
